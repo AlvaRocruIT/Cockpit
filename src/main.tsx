c12288bd614bc166cce4a62fe2bff4f9bb42a281
AlvaHRo <alvargash90@gmail.com>
2026-05-25 15:21:48 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
 import "./styles/index.css";
-import ".styles/app.css";
-import ".styles/font.css";
-import ".styles/tailwind.css";
-import ".styles/theme.css";
+import "./styles/app.css";
+import "./styles/font.css";
+import "./styles/tailwind.css";
+import "./styles/theme.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
