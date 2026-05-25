0c56ce9001e316962149cad6a5a85daabed475eb
AlvaHRo <alvargash90@gmail.com>
2026-05-25 12:02:22 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './styles/index.css'
 import './styles/fonts.css'
 import './styles/theme.css'
 import './styles/tailwind.css'
-import App from './App'
+import app from './app'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
