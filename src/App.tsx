b39da3023b487af8a0af33a85ea4de34df4fad80
AlvaHRo <alvargash90@gmail.com>
2026-06-06 13:11:19 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1343,7 +1343,6 @@ setSprints(reconstructedSprints);
   try {
     const response = await fetch(
   `${import.meta.env.VITE_API_URL}/projects`,
-  );
       {
         method: "POST",
         headers: {
